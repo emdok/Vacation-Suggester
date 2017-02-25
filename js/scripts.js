31e9a07647574bf144976dae41e3e90199c3c255
Emily Dokken <dokkenemily@gmail.com>
2017-02-25 09:29:11 -0800

    add input variables for each select in the form within the javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var personNameInput = $("input#personName").val();
+    var questionOneInput = $("#question1").val();
+    var questionTwoInput = $("#question2").val();
+    var questionThreeInput = $("#question3").val();
+    var questionFourInput = $("#question4").val();
+    var questionFiveInput = $("#question5").val();
+    console.log(questionOneInput);
+    console.log(questionTwoInput);
+    console.log(questionThreeInput);
+    console.log(questionFourInput);
+    console.log(questionFiveInput);
 
     $(".personResult").text(personNameInput);
 
