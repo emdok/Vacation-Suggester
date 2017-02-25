7ddfb38ecb599eab752d237265897b65423fcd6c
Emily Dokken <dokkenemily@gmail.com>
2017-02-25 09:55:00 -0800

    add simple branching to javascript file using equal to if statments

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var personNameInput = $("input#personName").val();
-    var questionOneInput = $("#question1").val();
-    var questionTwoInput = $("#question2").val();
-    var questionThreeInput = $("#question3").val();
-    var questionFourInput = $("#question4").val();
-    var questionFiveInput = $("#question5").val();
-    console.log(questionOneInput);
-    console.log(questionTwoInput);
-    console.log(questionThreeInput);
-    console.log(questionFourInput);
-    console.log(questionFiveInput);
+    var qOneInput = parseInt($("#question1").val());
+    var qTwoInput = parseInt($("#question2").val());
+    var qThreeInput = parseInt($("#question3").val());
+    var qFourInput = parseInt($("#question4").val());
+    var qFiveInput = parseInt($("#question5").val());
+
+    if (qOneInput === 1 && qTwoInput === 1 && qThreeInput === 1 && qFourInput === 1 && qFiveInput === 1) {
+      $("#NorthAmerica").show();
+    }
+
+    if (qOneInput === 2 && qTwoInput === 2 && qThreeInput === 2 && qFourInput === 2 && qFiveInput === 2) {
+      $("#Asia").show();
+    }
+
+    if (qOneInput === 3 && qTwoInput === 3 && qThreeInput === 3 && qFourInput === 3 && qFiveInput === 3) {
+      $("#Europe").show();
+    }
+
 
     $(".personResult").text(personNameInput);
+    $("#result").show();
+
 
     event.preventDefault();
   });
